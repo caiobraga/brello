73f6b95281731fc12d73dec7f0aa43f2bb024cba
jonhpolsh <jo1693.pzoli@gmail.com>
2023-06-24 19:09:41 -0300

    modified:   src/ui/features/projects/index.js

--- src/ui/features/projects/index.js
+++ src/ui/features/projects/index.js
@@ -1,40 +1,70 @@
-import React, { useState } from 'react';
-import Navbar  from '../../components/navbar';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import Navbar from "../../components/navbar";
+
+import { Box, Button, Flex, Grid, Image } from "@chakra-ui/react";
+// import "./style.css"; // Import the CSS file
+
+const ProjectCell = ({name, info}) => {
+    return (
+        <Flex width={240} border='2px solid black' boxShadow='xl' rounded='md' flexDirection='column'>
+            <Flex flexDirection='column'>
+                <h1>{name}</h1>
+                <h3>{info}</h3>
+            </Flex>
+        </Flex>
+    );
+};
 
 const ProjectScreen = () => {
-  const [projectName, setProjectName] = useState('');
+	const history = useHistory();
 
-  const handleInputChange = (event) => {
-    setProjectName(event.target.value);
-  };
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const handleEmailChange = (event) => {
+		setEmail(event.target.value);
+	};
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Perform project creation logic here
-    // using the entered project name
-    console.log(`Creating project: ${projectName}`);
-    setProjectName('');
-  };
+	const handlePasswordChange = (event) => {
+		setPassword(event.target.value);
+	};
 
-  return (
-    <div>
-         <Navbar />
-      <h2>Create Project</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Project Name:
-          <input
-            type="text"
-            value={projectName}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <button type="submit">Create</button>
-      </form>
-    </div>
-  );
+	const handleButtonClick = () => {
+		history.push("/dashbord");
+	};
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		// Perform login logic here, such as making an API call
+		// with the entered email and password
+
+		// Reset the form
+		setEmail("");
+		setPassword("");
+	};
+
+	return (
+		<div className="App">
+			<Navbar></Navbar>
+            <Flex flexStart px='14px' gap='14px'>
+                <ProjectCell name='teste' info='Lorem ipsum dolor simet, Lorem ipsum dolor simet, Lorem ipsum dolor simet, Lorem ipsum dolor simet, Lorem ipsum dolor simet'></ProjectCell>
+                <ProjectCell name='teste' info='Lorem ipsum dolor simets'></ProjectCell>
+            </Flex>
+            {/* <Grid px='auto' justifyContent='center' gridRowGap='14px' gridColumnGap='14px' gridAutoFlow='row dense' gridTemplateColumns='repeat(auto-fill, 240px)'>
+                <ProjectCell name='teste' info='Lorem ipsum dolor simets'></ProjectCell>
+                <ProjectCell name='teste' info='Lorem ipsum dolor simets'></ProjectCell>
+                <ProjectCell name='teste' info='Lorem ipsum dolor simets'></ProjectCell>
+                <ProjectCell name='teste' info='Lorem ipsum dolor simets'></ProjectCell>
+                <ProjectCell name='teste' info='Lorem ipsum dolor simets'></ProjectCell>
+                <ProjectCell name='teste' info='Lorem ipsum dolor simets'></ProjectCell>
+                <ProjectCell name='teste' info='Lorem ipsum dolor simets'></ProjectCell>
+                <ProjectCell name='teste' info='Lorem ipsum dolor simets'></ProjectCell>
+                <ProjectCell name='teste' info='Lorem ipsum dolor simets'></ProjectCell>
+            </Grid> */}
+		</div>
+	);
 };
 
 export default ProjectScreen;
-
