dee25ac709a1ce0506ea7f8c4b078a41e33235fa
Caio Gomes Braga <45905947+caiobraga@users.noreply.github.com>
2023-06-24 16:01:16 -0300

    safsd

--- src/ui/features/login/index.js
+++ src/ui/features/login/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-
+import { NavBar } from '../../components/navbar';
 const LoginScreen = () => {
     const history = useHistory();
 
