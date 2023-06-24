baebee7630c8a25c659669eab3c26c8681a18221
Caio Gomes Braga <45905947+caiobraga@users.noreply.github.com>
2023-06-24 16:55:53 -0300

    asdfsaf

--- src/ui/components/navbar/index.js
+++ src/ui/components/navbar/index.js
@@ -1,33 +1,20 @@
 import React from 'react';
-import { Flex, Box, Text, Spacer, Link, IconButton } from '@chakra-ui/react';
-import { AiFillHome, AiOutlineInfoCircle, AiOutlineMail } from 'react-icons/ai';
-import { MoonIcon } from '@chakra-ui/icons';
+import { Flex, Box, Text, Spacer } from '@chakra-ui/react';
 
 const Navbar = () => {
   return (
-    <Flex align="center" justify="space-between" p={4} bg="teal.500" color="white">
-      <Box>
-        <Text fontSize="xl" fontWeight="bold">
-          Brello
-        </Text>
-      </Box>
-      <Flex align="center">
-        <Link as="button" mr={4} fontSize="lg">
-          <AiFillHome />
-        </Link>
-        <Link as="button" mr={4} fontSize="lg">
-          <AiOutlineInfoCircle />
-        </Link>
-        <Link as="button" mr={4} fontSize="lg">
-          <AiOutlineMail />
-        </Link>
-        <IconButton
-          aria-label="Toggle Dark Mode"
-          icon={<MoonIcon />}
-          variant="ghost"
-        />
-      </Flex>
+    <Flex align="center" p={4} bg="teal.500" color="Black">
+    <Box>
+      <Text fontSize="xl" fontWeight="bold">
+        Brello
+      </Text>
+    </Box>
+    <Flex ml="auto">
+      <Text mr={4}>Home</Text>
+      <Text mr={4}>About</Text>
+      <Text mr={4}>Contact</Text>
     </Flex>
+  </Flex>
   );
 };
 
