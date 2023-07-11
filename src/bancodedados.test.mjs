a8cad3d895280249dcd4314af04602ed0529cdfb
Henrique <henrique2801lopes@gmail.com>
2023-07-11 19:37:06 -0300

    Teste Login

--- src/bancodedados.test.mjs
+++ src/bancodedados.test.mjs
@@ -9,28 +9,39 @@ describe('Database', () => {
     const database = new Database();
   });
 
-  test('should create a new project', () => {
+  // test('should create a new project', () => {
     
 
-    const projeto = new Projeto('Projeto 1', 'Descrição do Projeto 1', 'Gerente 1');
-    database.addProjectInDatabase(projeto);
+  //   const projeto = new Projeto('Projeto 1', 'Descrição do Projeto 1', 'Gerente 1');
+  //   database.addProjectInDatabase(projeto);
 
-    const projects = database.fetchProjectsFromDatabase();
+  //   const projects = database.fetchProjectsFromDatabase();
 
-    expect(projects.length).toBe(1);
-    expect(projects[0].nome).toBe('Projeto 1');
-    expect(projects[0].descricao).toBe('Descrição do Projeto 1');
-    expect(projects[0].gerente).toBe('Gerente 1');
-  });
+  //   expect(projects.length).toBe(1);
+  //   expect(projects[0].nome).toBe('Projeto 1');
+  //   expect(projects[0].descricao).toBe('Descrição do Projeto 1');
+  //   expect(projects[0].gerente).toBe('Gerente 1');
+  // });
 
   test('should create a new project', () => {
     const database = new Database();
-    const user = new User('Emailteste', 'Senhateste', 'NomeTeste', 'numeroTeste', 'cpfTeste');
+    const user = new User('emailteste@gmail.com', 'Senhateste', 'NomeTeste', 'numeroTeste', '11111');
+    database.addUserLista(user);
 
     const listaUser = database.getListaUsuarios();
+    const tamanho = listaUser.length;
+
+    const nomeListaUser = listaUser[0].name;
+    const cpf = listaUser[0].cpf;
+    const email = listaUser[0].email;
+    const senha = listaUser[0].password;
+
+    expect(tamanho).toBe(1);
+    expect(nomeListaUser).toBe('NomeTeste');
+    expect(cpf).toBe('11111');
+    expect(email).toBe('emailteste@gmail.com');
+    expect(senha).toBe('Senhateste');
     
-    expect(listaUser.length).toBe(1);
-    expect(listaUser.User.name).tobe('NomeTeste')
   });
 
 
